0e49cc636edbccfc365fa15e0c5994430a5d8b71
nezu <nezu@nezu.(none)>
2013-06-17 09:13:12 -0400

    first

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+@name           Victoria
+@description    Challenge 0
+*
+*
+*
+*/
+
+int main()
+{
+
+int i;
+int c = 0; //counter
+
+    for(i = 2; i< 223; ++i)
+    {
+
+        if((i % 3 == 0 && i % 4 != 0) || ( i % 3 != 0 && i % 4 == 0))
+        {//logical xor
+            continue;
+        }
+        else if(i % 3 == 0 && i % 4 == 0)
+                {
+                    printf("A100 Rocks!!\n");
+                    c++;
+
+                }
+            else if (i % 4 == 0)
+                {
+                    printf("A100\n");
+                }
+                  else
+                {
+                    printf("%d\n",i);
+                }
+    }
+                    printf("A100 Rocks %d Times \n", c);
+return 0;
+}
